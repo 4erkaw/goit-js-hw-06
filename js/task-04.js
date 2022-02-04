0eff81755fe31e68b59a09b0872150c98253ce81
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-02-04 14:21:58 +0200

    Я не понимаю что нужно сделать так что пока так

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,20 @@
 const refs = {
   decBtn: document.querySelector('[data-action="decrement"]'),
   incBtn: document.querySelector('[data-action="increment"]'),
-  counter: document.querySelector("#value"),
+  counter: document.querySelector("span"),
 };
+let counterValue = Number(refs.counter.textContent)
+counterValue = 0
 
 const decrementValue = () => {
-  return (refs.counter.textContent -= 1);
+   counterValue -= 1;
+   return refs.counter.textContent = counterValue
 };
 const incrementValue = () => {
-  return (refs.counter.textContent = Number(refs.counter.textContent) + 1);
+  counterValue = Number(counterValue) + 1;
+   return refs.counter.textContent = counterValue
 };
+
 
 refs.decBtn.addEventListener("click", decrementValue);
 refs.incBtn.addEventListener("click", incrementValue);
